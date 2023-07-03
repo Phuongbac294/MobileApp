0be01012cdd9dde814a9ca7191de212a20a51b47
Phuongbac294 <110046513+Phuongbac294@users.noreply.github.com>
2023-07-03 17:55:58 +0700

    up

--- Lesson/express01/index.js
+++ Lesson/express01/index.js
@@ -1,13 +1,23 @@
 const fs = require('fs');
-const readFile = require('./src/lib/readFile');
+const readSyc = require('./src/lib/readFile');
 
- fs.readFile(
-    './src/database/text.txt',
-    'utf-8',
-    (err, re) => {
-        if (err) {console.log(err)};
-        console.log('reuslt',re);
-    }
- )
+//  fs.readFile(
+//     './src/database/text.txt',
+//     'utf-8',
+//     (err, re) => {
+//         if (err) {console.log(err)};
+//         console.log('reuslt',re);
+//     }
+//  )
+const main = async () => {
+    const data = readSyc('personnel.json', (reuslt) => {
+       console.log('result',reuslt);
+    });
+    console.log('data',data);
 
- readFile('personnel.js')
+    const dataResult = await readSyc('personnel.json');
+    console.log('dataResult',dataResult);
+}
+
+
+main();
--- Lesson/express01/src/lib/readFile.js
+++ Lesson/express01/src/lib/readFile.js
@@ -1,13 +1,25 @@
 const fs = require('fs');
 const path = require('path');
 
-const readFile = (filename, cb) => {
-    fs.readFile(path.join(__dirname, `../database/${filename}`), 'utf8',
-    (err, re) => {
-        if (err) {console.log(err)}
-        console.log(JSON.parse(re), re);
-        cb(JSON.parse(re));
-    })
-}
+// const read = (filename, cb) => {
+//     fs.readFile(path.join(__dirname, `../database/${filename}`), 'utf8',
+//     (err, re) => {
+//         if (err) {console.log(err)}
+//         // console.log(JSON.parse(re), re);
+//         cb(JSON.parse(re))
+//     })
+// }
 
-module.exports=readFile;
+const readSyc = async(filename) => {
+    try {
+        const data =  await fs.promises.readFile(
+        path.resolve(__dirname, `../database/${filename}`),
+        'utf8');
+            return JSON.parse(data);
+    }
+    catch (err) {
+        console.log(err);
+        return[]
+    }
+}
+module.exports= readSyc;
