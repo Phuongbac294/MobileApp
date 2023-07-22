88757d526eae26ae49b16a4bdb32340eb48c92e0
Phuongbac294 <110046513+Phuongbac294@users.noreply.github.com>
2023-07-22 17:56:01 +0700

    1

--- app/App.js
+++ app/App.js
@@ -1,64 +1,92 @@
-// import * as React from 'react'
-import { StatusBar } from 'expo-status-bar';
-// import { 
-//   Text, // span
-//   View, // div
-//   StyleSheet, // css
-//   TextInput, // input
-//   // click event 
-//   Touchable,  //
-//   TouchableOpacity,
-//   Button,
-//   Pressable,
-//   //Scroll or render list
-//   ScrollView,
-//   FlatList,
-//   //
-//   SafeAreaView,
-//   StatusBar,
-//   //
-//   Platform,
-//   // kích thước máy
-//   Dimensions,
-//   Alert,
-//   Image,
-//   //
-//   Animated,
-// } from 'react-native';
-// import Lesson from './src/lesson/lesson';
-// import { InputAccessoryView } from 'react-native-input-scrollable';
-
-// import axios from 'axios';
-
-// const {height, width} = Dimensions.get('window');
-import {View, Text, StyleSheet, Button} from 'react-native';
 import React from 'react';
+import {StyleSheet, View, Text, Button} from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default function App() {
-  const [count, setCount] = React.useState(0);
-  const RenderText = ({conut}) => {
-    return (
-      <Text style={styles.textStyle}>State : {count+3}</Text>
-    )
-  }
-  
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+
+function Home() {
+  const navigation = useNavigation();
   return (
-    <View style={styles.container}>  
-      <Text style={styles.textStyle}>Count: {count}</Text>
-      <RenderText/>
-      <RenderTextNew number = {count}/>
-      <Button
-        onPress={() => setCount(count+1)}
-        title ="Increase"/>      
+    <View style={styles.textStyle}>
+      <Text>Home</Text>
+      <Button title="Go to detail" onPress={() => {
+				navigation.navigate('Detail', {user: 'Alice'});
+			}} />
     </View>
   );
 }
 
-const RenderTextNew = ({number}) => {
-  return (
-    <Text style={styles.textStyle}>Number : {number+5}</Text>
-  )
+const Detail = () => {
+  const navigation = useNavigation();
+  const route = useRoute();
+	return (
+		<View style={styles.textStyle}>
+      <Text>Details Screen</Text>
+      <Text> User: {route.params.user}</Text>
+      <Button title="Go back" onPress={() => {
+				navigation.goBack()}} />
+      <Button title="Change Header" onPress={() => {
+				navigation.setOptions({title: 'Updated'})
+			}} />
+    </View>
+	)
 }
+
+function Settings() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+
+const HomeStack = createNativeStackNavigator();
+
+function HomeStackScreen() {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home" component={Home} />
+      <HomeStack.Screen name="Details" component={Detail} />
+    </HomeStack.Navigator>
+  );
+}
+
+const SettingsStack = createNativeStackNavigator();
+
+function SettingsStackScreen() {
+  return (
+    <SettingsStack.Navigator>
+      <SettingsStack.Screen name="Settings" component={Settings} />
+      <SettingsStack.Screen name="Details" component={Detail} />
+    </SettingsStack.Navigator>
+  );
+}
+
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      {/* <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} options={{ title: 'My home', headerStyle: {backgroundColor: '#f4511e', headerTitleStyle: {fontWeight: 'bold',}}, }}/>
+        <Stack.Screen name="Detail" component={Detail} />
+      </Stack.Navigator> */}
+      <Tab.Navigator >
+        <Tab.Screen name="Home" component={HomeStackScreen} options={{ tabBarBadge: 3 }}/>
+        <Tab.Screen name="Settings" component={SettingsStackScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
+// Style CSS
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -66,5 +94,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  textStyle: {fontSize: 19,fontWeight: 'bold', textAlign: 'center', color: 'red'},
+  // textStyle: {fontSize: 19,fontWeight: 'bold', textAlign: 'center', color: 'red'},
+  textStyle: { flex: 1, alignItems: 'center', justifyContent: 'center' }
 });
+
--- app/src/lesson/lesson.js
+++ app/src/lesson/lesson.js
@@ -1,10 +1,75 @@
+import {View, Text, StyleSheet, Button} from 'react-native';
+import React from 'react';
+// import { 
+//   Text, // span
+//   View, // div
+//   StyleSheet, // css
+//   TextInput, // input
+//   // click event 
+//   Touchable,  //
+//   TouchableOpacity,
+//   Button,
+//   Pressable,
+//   //Scroll or render list
+//   ScrollView,
+//   FlatList,
+//   //
+//   SafeAreaView,
+//   StatusBar,
+//   //
+//   Platform,
+//   // kích thước máy
+//   Dimensions,
+//   Alert,
+//   Image,
+//   //
+//   Animated,
+// } from 'react-native';
+// import Lesson from './src/lesson/lesson';
+// import { InputAccessoryView } from 'react-native-input-scrollable';
+
+// import axios from 'axios';
+
+// const {height, width} = Dimensions.get('window');
 
 function Lesson() {
-    return (
-        <div className = "Lesson">
-            Phần lesson
-        </div>
-    )
+   const [count, setCount] = React.useState(0);
+    const RenderText = ({conut}) => {
+        return (
+        <Text style={styles.textStyle}>State : {count+3}</Text>
+        )
+    }
+  
+  return (
+    <View style={styles.container}>  
+      <Text style={styles.textStyle}>Count: {count}</Text>
+      {/* <RenderText/>
+      <RenderTextNew number = {count}/> */}
+      <RenderCount a={count}>
+        <Text style={styles.textStyle}>hi am children {count}</Text>
+      </RenderCount>
+      <RenderCount a={count}>
+        <Text style={styles.textStyle}>hi am first {count}</Text>
+      </RenderCount>
+      <Button
+        onPress={() => setCount(count+1)}
+        title ="Increase"/>      
+    </View>
+  );
 }
+
+const RenderCount = ({a, children}) => {
+    const [count, setCount] = React.useState(a);
+    React.useEffect (() => {setCount(a);}, []);// dùng Effect giúp children có thể nhận được tác động từ cha, nếu ko có thì sẽ độc lập ko nhận dc.
+    return (
+      <>
+      <Text style={styles.textStyle}>{count}</Text>
+      <Button
+          onPress={() => setCount(count+1)}
+          title ="Increase children"/>
+      {children}
+      </>
+    )
+  }
 
 export default Lesson;
